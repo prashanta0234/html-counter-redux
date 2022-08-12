6adf8306742a0f728f19de0bdbb374e133727c4b
prashanta0234 <prashanta0234@gmail.com>
2022-08-13 03:56:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+// // select element
+
+// const counterEl = document.getElementById("counter");
+// const incrimentEl = document.getElementById("incriment");
+// const decrementEl = document.getElementById("decrement");
+
+// // initial state
+// const initialState = {
+//   value: 0,
+// };
+
+// // create redux reducer funtion
+// function counterReducer(state = initialState, action) {
+//   if (action.type === "incriment") {
+//     return {
+//       ...state,
+//       value: state.value + 1,
+//     };
+//   } else if (action.type === "decrement") {
+//     return {
+//       ...state,
+//       value: state.value - 1,
+//     };
+//   } else {
+//     return state;
+//   }
+// }
+
+// // create store
+// const store = Redux.createStore(counterReducer);
+
+// // create render function and show data in ui
+
+// const render = () => {
+//   const state = store.getState();
+//   counterEl.innerText = state.value.toString();
+// };
+// render();
+// //  subscribe
+// store.subscribe(render);
+
+// //  button listeners
+
+// incrimentEl.addEventListener("click", () => {
+//   store.dispatch({
+//     type: "incriment",
+//   });
+// });
+
+// decrementEl.addEventListener("click", () => {
+//   store.dispatch({
+//     type: "decrement",
+//   });
+// });
+
+const counterEl = document.getElementById("counter");
+const inrementEl = document.getElementById("incriment");
+const decrementEl = document.getElementById("decrement");
+
+const initialState = {
+  value: 0,
+};
+
+// reduccer function
+
+function reducerFun(state = initialState, action) {
+  if (action.type === "inrement") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decriemnt") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+}
+
+// store
+const store = Redux.createStore(reducerFun);
+
+const render = () => {
+  const state = store.getState();
+  counterEl.innerText = state.value.toString();
+};
+
+store.subscribe(render);
+
+decrementEl.addEventListener("click", () => {
+  store.dispatch({
+    type: "decriemnt",
+  });
+});
+
+inrementEl.addEventListener("click", () => {
+  store.dispatch({
+    type: "inrement",
+  });
+});
